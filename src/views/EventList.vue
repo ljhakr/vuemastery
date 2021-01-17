37a0c52d6863d6042149398bf9c9f487d9d069ba
ljhakr <77569979+ljhakr@users.noreply.github.com>
2021-01-17 11:18:34 +0100

    Add files via upload

---
+++ src/views/EventList.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>Events to make the world better</h1>
+  <div class="event">
+    <EventCard
+        v-for="event in events"
+        :key="event.id"
+        :event="event"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue';
+import EventService from '@/services/EventService';
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null,
+    };
+  },
+  created() {
+    EventService.getEvents().then(response => {
+      this.events = response.data;
+    }).catch(error => {
+      console.log(error);
+    });
+  },
+};
+</script>
+
+<style>
+.event {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+</style>
