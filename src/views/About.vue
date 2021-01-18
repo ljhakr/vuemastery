ca961663696b1e13177b41b67679fcef76644ac2
ljhakr <77569979+ljhakr@users.noreply.github.com>
2021-01-18 22:03:14 +0100

    Add files via upload

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>A site with events to make the world a better place</h1>
+    <h1>A site with events to make the world a better place right now</h1>
   </div>
 </template>
